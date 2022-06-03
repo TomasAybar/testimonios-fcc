d6ef03f029fa1f30802817c186a786ee9786cdc7
tomasAybar <aybar.t4@gmail.com>
2022-06-03 17:01:09 -0300

    proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -6,7 +6,37 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
         <h1 className='titulo-principal'>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimonio />
+
+        <Testimonio
+          nombre='Shawn Wang'
+          pais='Singapore'
+          imagen='shawn'
+          cargo='Ingeniero de Software'
+          empresa='Amazon'
+          testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.' />
+
+        <Testimonio
+          nombre='Sarah Chima'
+          pais='Nigeria'
+          imagen='sarah'
+          cargo='Ingeniera de Software'
+          empresa='ChatDesk'
+          testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.' />
+
+        <Testimonio
+          nombre='Emma Bostian'
+          pais='Suecia'
+          imagen='emma'
+          cargo='Ingeniera de Software'
+          empresa='Spotify'
+          testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.' />
+        <Testimonio
+          nombre='Tomas Aybar'
+          pais='Argentina'
+          imagen='tomas'
+          cargo='Full Stack MERN'
+          empresa='una Empresa 🦾'
+          testimonio='Esto es una prueba de componentes personalizados utilizando props para cambiar dinamicamente la informacion de cada testimonio. Este es el primer proyecto del curso React Desde Cero de FreeCodeCamp 💓' />
       </div>
     </div>
   );
